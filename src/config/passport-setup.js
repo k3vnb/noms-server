8a9c9610e4713d49cf84eb6aac4529809d9543f4
Kevin B <boyle.kevin.michael@gmail.com>
2024-06-18 22:24:46 -0700

    update project name, add err catch to passport setup

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -27,29 +27,34 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       // eslint-disable-next-line no-console
       console.log("Connecting to Google...");
-      AuthService.findUserById(db, profile.id).then(async usr => {
-        if (usr) return done(null, usr);
-        /*
-            in our db, user_name can only be unique,
-            however the Google displayName is not necessarily unique
-            to avoid an error if two users with, ie, displayName 'John Smith' try to join,
-            we first check if username exists,
-            if so, we get a unique number to concat on the displayName
-        */
-        const newProfile = { ...profile };
-        const checkUsrNameExists = await UsersService.checkUserNameExists(
-          db,
-          profile.displayName
-        );
-        if (checkUsrNameExists[0]) {
-          const uniqueId = await UsersService.getHighestId(db);
-          newProfile.displayName = `${profile.displayName} ${uniqueId[0].id +
-            1}`;
-        }
-        AuthService.createUser(db, newProfile).then(newUsr => {
-          return done(null, newUsr[0]);
+      AuthService.findUserById(db, profile.id)
+        .then(async usr => {
+          if (usr) return done(null, usr);
+          /*
+              in our db, user_name can only be unique,
+              however the Google displayName is not necessarily unique
+              to avoid an error if two users with, ie, displayName 'John Smith' try to join,
+              we first check if username exists,
+              if so, we get a unique number to concat on the displayName
+          */
+          const newProfile = { ...profile };
+          const checkUsrNameExists = await UsersService.checkUserNameExists(
+            db,
+            profile.displayName
+          );
+          if (checkUsrNameExists[0]) {
+            const uniqueId = await UsersService.getHighestId(db);
+            newProfile.displayName = `${profile.displayName} ${uniqueId[0].id +
+              1}`;
+          }
+          AuthService.createUser(db, newProfile).then(newUsr => {
+            return done(null, newUsr[0]);
+          });
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          console.log("Error in passport-setup.js", err);
         });
-      });
     }
   )
 );
