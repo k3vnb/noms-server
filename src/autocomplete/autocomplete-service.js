2ef8c3cea73b57b0b475c6d4721571e4741ca222
Kevin B <boyle.kevin.michael@gmail.com>
2024-06-30 19:23:06 -0700

    update uuid pkg

--- src/autocomplete/autocomplete-service.js
+++ src/autocomplete/autocomplete-service.js
@@ -1,8 +1,8 @@
-const { uuid } = require("uuidv4");
+const uuid = require("uuid");
 const fetch = require("node-fetch");
 
 const autocompleteService = {
-  async fetchResults(query, sessionToken = uuid()) {
+  async fetchResults(query, sessionToken = uuid.v4()) {
     const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURI(
       query
     )}&types=establishment&location=45.5230,-122.6675&radius=16093&strictbounds&offset=3&key=${
