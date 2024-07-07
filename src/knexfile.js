c23d5ea44ca7c46b174feda0eea6c0a0e774fcb4
Kevin B <boyle.kevin.michael@gmail.com>
2024-07-07 16:09:28 -0700

    update knex config

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,7 +1,9 @@
 module.exports = {
   client: "pg",
-  connectionString: process.env.PG_CONNECTION_STRING,
-  ssl: {
-    rejectUnauthorized: false
+  connection: {
+    connectionString: process.env.PG_CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 };
